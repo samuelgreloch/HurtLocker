8141c27ce6b51042f1c36320c10222dc343a4204
sgreloch <samuelgreloch@yahoo.com>
2024-12-09 14:07:54 -0500

    more

--- src/main/java/JerkSonParser.java
+++
@@ -1,8 +0,0 @@
-import java.util.Map;
-
-public class JerkSonParser {
-    public static Map<String, String> parseRecord(String input) {
-        return null;
-    }
-
-}
--- src/test/java/RawDataTest.java
+++ src/test/java/RawDataTest.java
@@ -1,11 +1,13 @@
 import org.apache.commons.io.IOUtils;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.FileReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 // const matches = 'Some Text'.match(regex);
@@ -19,20 +21,35 @@ import static org.junit.Assert.assertTrue;
 
 public class RawDataTest {
 
+    private static String RawData;
+
+    @Before
+    public void setup() throws Exception {
+        RawData = (new Main()).readRawDataToString();
+    }
 
 @Test
     public void testRecords() {
-    String input = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016";
+    String input = RawData;
 
-    Map<String, String> expectedMap = new HashMap<>();
-    expectedMap.put("naMe", "Milk");
-    expectedMap.put("price", "3.23");
-    expectedMap.put("type", "food");
-    expectedMap.put("experition", "1/24/2016");
+    JerkSonParser parser = new JerkSonParser();
+    parser.readRawData(input);
+    parser.parseData();
 
-    Map<String, String> actualMap = JerkSonParser.parseRecord(input);
+    List<JerkSonParser> parsedData = parser.getParsedData();
 
-    assertEquals(expectedMap, actualMap);
+
+    assertNotNull(parsedData);
+    assertTrue(parsedData.size()>0);
+
+    assertEquals("Name:Milk",parsedData.get(0).toString());
+    assertEquals("Price:3.23",parsedData.get(1).toString());
+
+
+
+
+
+
 }
 
     @Test
